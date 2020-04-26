a0698be5aa40b868c1fd8d233e98d76239379088
Roman Nayda <rpn5082@psu.edu>
2020-04-26 18:12:55 -0400

    Database and Doctor screen

--- DentalHospitalDatabase/Doctor.xaml.cs
+++ DentalHospitalDatabase/Doctor.xaml.cs
@@ -22,26 +22,44 @@ namespace DentalHospitalDatabase
         public Doctor()
         {
             InitializeComponent();
+
+            AppDataGrid.Opacity = 0.0;
+            PatientDataGrid.Opacity = 0.0;
+            DocDataGrid.Opacity = 0.0;
+            BillingDataGrid.Opacity = 0.0;
+
         }
 
         private void btnPatients_Click(object sender, RoutedEventArgs e)
         {
-
+            PatientDataGrid.Opacity = 100.0;
+            AppDataGrid.Opacity = 0.0;
+            DocDataGrid.Opacity = 0.0;
+            BillingDataGrid.Opacity = 0.0;
         }
 
         private void btnApp_Click(object sender, RoutedEventArgs e)
         {
-
+            PatientDataGrid.Opacity = 0.0;
+            AppDataGrid.Opacity = 100.0;
+            DocDataGrid.Opacity = 0.0;
+            BillingDataGrid.Opacity = 0.0;
         }
 
         private void btnDoctors_Click(object sender, RoutedEventArgs e)
         {
-
+            PatientDataGrid.Opacity = 0.0;
+            AppDataGrid.Opacity = 0.0;
+            DocDataGrid.Opacity = 100.0;
+            BillingDataGrid.Opacity = 0.0;
         }
 
         private void btnBilling_Click(object sender, RoutedEventArgs e)
         {
-
+            PatientDataGrid.Opacity = 0.0;
+            AppDataGrid.Opacity = 0.0;
+            DocDataGrid.Opacity = 0.0;
+            BillingDataGrid.Opacity = 100.0;
         }
 
         private void btnLogOut_Click(object sender, RoutedEventArgs e)
@@ -49,6 +67,45 @@ namespace DentalHospitalDatabase
             Login_Screen login_Screen = new Login_Screen();
             login_Screen.Show();
             this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            DentalHospitalDatabase.dbHospitalDataSet dbHospitalDataSet = ((DentalHospitalDatabase.dbHospitalDataSet)(this.FindResource("dbHospitalDataSet")));
+            // Load data into the table patient. You can modify this code as needed.
+            DentalHospitalDatabase.dbHospitalDataSetTableAdapters.patientTableAdapter dbHospitalDataSetpatientTableAdapter = new DentalHospitalDatabase.dbHospitalDataSetTableAdapters.patientTableAdapter();
+            dbHospitalDataSetpatientTableAdapter.Fill(dbHospitalDataSet.patient);
+            System.Windows.Data.CollectionViewSource patientViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("patientViewSource")));
+            patientViewSource.View.MoveCurrentToFirst();
+            DentalHospitalDatabase.dbHospitalDataSet1 dbHospitalDataSet1 = ((DentalHospitalDatabase.dbHospitalDataSet1)(this.FindResource("dbHospitalDataSet1")));
+            // Load data into the table scheduling. You can modify this code as needed.
+            DentalHospitalDatabase.dbHospitalDataSet1TableAdapters.schedulingTableAdapter dbHospitalDataSet1schedulingTableAdapter = new DentalHospitalDatabase.dbHospitalDataSet1TableAdapters.schedulingTableAdapter();
+            dbHospitalDataSet1schedulingTableAdapter.Fill(dbHospitalDataSet1.scheduling);
+            System.Windows.Data.CollectionViewSource schedulingViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("schedulingViewSource")));
+            schedulingViewSource.View.MoveCurrentToFirst();
+            DentalHospitalDatabase.dbHospitalDataSet2 dbHospitalDataSet2 = ((DentalHospitalDatabase.dbHospitalDataSet2)(this.FindResource("dbHospitalDataSet2")));
+            // Load data into the table scheduling. You can modify this code as needed.
+            DentalHospitalDatabase.dbHospitalDataSet2TableAdapters.schedulingTableAdapter dbHospitalDataSet2schedulingTableAdapter = new DentalHospitalDatabase.dbHospitalDataSet2TableAdapters.schedulingTableAdapter();
+            dbHospitalDataSet2schedulingTableAdapter.Fill(dbHospitalDataSet2.scheduling);
+            System.Windows.Data.CollectionViewSource schedulingViewSource1 = ((System.Windows.Data.CollectionViewSource)(this.FindResource("schedulingViewSource1")));
+            schedulingViewSource1.View.MoveCurrentToFirst();
+            // Load data into the table doctor. You can modify this code as needed.
+            DentalHospitalDatabase.dbHospitalDataSet2TableAdapters.doctorTableAdapter dbHospitalDataSet2doctorTableAdapter = new DentalHospitalDatabase.dbHospitalDataSet2TableAdapters.doctorTableAdapter();
+            dbHospitalDataSet2doctorTableAdapter.Fill(dbHospitalDataSet2.doctor);
+            System.Windows.Data.CollectionViewSource doctorViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("doctorViewSource")));
+            doctorViewSource.View.MoveCurrentToFirst();
+            DentalHospitalDatabase.dbHospitalDataSet3 dbHospitalDataSet3 = ((DentalHospitalDatabase.dbHospitalDataSet3)(this.FindResource("dbHospitalDataSet3")));
+            // Load data into the table billing. You can modify this code as needed.
+            DentalHospitalDatabase.dbHospitalDataSet3TableAdapters.billingTableAdapter dbHospitalDataSet3billingTableAdapter = new DentalHospitalDatabase.dbHospitalDataSet3TableAdapters.billingTableAdapter();
+            dbHospitalDataSet3billingTableAdapter.Fill(dbHospitalDataSet3.billing);
+            System.Windows.Data.CollectionViewSource billingViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("billingViewSource")));
+            billingViewSource.View.MoveCurrentToFirst();
+            // Load data into the table doctor. You can modify this code as needed.
+            DentalHospitalDatabase.dbHospitalDataSet3TableAdapters.doctorTableAdapter dbHospitalDataSet3doctorTableAdapter = new DentalHospitalDatabase.dbHospitalDataSet3TableAdapters.doctorTableAdapter();
+            dbHospitalDataSet3doctorTableAdapter.Fill(dbHospitalDataSet3.doctor);
+            System.Windows.Data.CollectionViewSource doctorViewSource1 = ((System.Windows.Data.CollectionViewSource)(this.FindResource("doctorViewSource1")));
+            doctorViewSource1.View.MoveCurrentToFirst();
         }
     }
 }
