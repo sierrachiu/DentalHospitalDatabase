0657ecbccd414f02b00d491aaa629f2bed7e7d74
Roman Nayda <rpn5082@psu.edu>
2020-04-26 20:10:55 -0400

    Doctor screen database changes

--- DentalHospitalDatabase/Doctor.xaml.cs
+++ DentalHospitalDatabase/Doctor.xaml.cs
@@ -24,26 +24,63 @@ namespace DentalHospitalDatabase
             InitializeComponent();
 
             PatientDataGrid.Opacity = 0;
+            BillingDataGrid.Opacity = 0;
+            DocDataGrid.Opacity = 0;
+            AppDataGrid.Opacity = 0;
         }
 
         private void btnPatient_Click(object sender, RoutedEventArgs e)
         {
+            Panel.SetZIndex(PatientDataGrid, 3);
+            Panel.SetZIndex(BillingDataGrid, 2);
+            Panel.SetZIndex(DocDataGrid, 1);
+            Panel.SetZIndex(AppDataGrid, 0);
+
             PatientDataGrid.Opacity = 1;
+            BillingDataGrid.Opacity = 0;
+            DocDataGrid.Opacity = 0;
+            AppDataGrid.Opacity = 0;
+
         }
 
         private void btnBilling_Click(object sender, RoutedEventArgs e)
         {
+            Panel.SetZIndex(PatientDataGrid, 2);
+            Panel.SetZIndex(BillingDataGrid, 3);
+            Panel.SetZIndex(DocDataGrid, 1);
+            Panel.SetZIndex(AppDataGrid, 0);
+
+            PatientDataGrid.Opacity = 0;
+            BillingDataGrid.Opacity = 1;
+            DocDataGrid.Opacity = 0;
+            AppDataGrid.Opacity = 0;
 
         }
 
         private void btnDoctor_Click(object sender, RoutedEventArgs e)
         {
+            Panel.SetZIndex(PatientDataGrid, 1);
+            Panel.SetZIndex(BillingDataGrid, 2);
+            Panel.SetZIndex(DocDataGrid, 3);
+            Panel.SetZIndex(AppDataGrid, 0);
 
+            PatientDataGrid.Opacity = 0;
+            BillingDataGrid.Opacity = 0;
+            DocDataGrid.Opacity = 1;
+            AppDataGrid.Opacity = 0;
         }
 
         private void btnApp_Click(object sender, RoutedEventArgs e)
         {
+            Panel.SetZIndex(PatientDataGrid, 0);
+            Panel.SetZIndex(BillingDataGrid, 1);
+            Panel.SetZIndex(DocDataGrid, 2);
+            Panel.SetZIndex(AppDataGrid, 3);
 
+            PatientDataGrid.Opacity = 0;
+            BillingDataGrid.Opacity = 0;
+            DocDataGrid.Opacity = 0;
+            AppDataGrid.Opacity = 1;
         }
 
         private void btnLogOut_Click(object sender, RoutedEventArgs e)
@@ -62,6 +99,21 @@ namespace DentalHospitalDatabase
             hospitalDataSetpatientTableAdapter.Fill(hospitalDataSet.patient);
             System.Windows.Data.CollectionViewSource patientViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("patientViewSource")));
             patientViewSource.View.MoveCurrentToFirst();
+            // Load data into the table billing. You can modify this code as needed.
+            DentalHospitalDatabase.HospitalDataSetTableAdapters.billingTableAdapter hospitalDataSetbillingTableAdapter = new DentalHospitalDatabase.HospitalDataSetTableAdapters.billingTableAdapter();
+            hospitalDataSetbillingTableAdapter.Fill(hospitalDataSet.billing);
+            System.Windows.Data.CollectionViewSource billingViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("billingViewSource")));
+            billingViewSource.View.MoveCurrentToFirst();
+            // Load data into the table doctor. You can modify this code as needed.
+            DentalHospitalDatabase.HospitalDataSetTableAdapters.doctorTableAdapter hospitalDataSetdoctorTableAdapter = new DentalHospitalDatabase.HospitalDataSetTableAdapters.doctorTableAdapter();
+            hospitalDataSetdoctorTableAdapter.Fill(hospitalDataSet.doctor);
+            System.Windows.Data.CollectionViewSource doctorViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("doctorViewSource")));
+            doctorViewSource.View.MoveCurrentToFirst();
+            // Load data into the table scheduling. You can modify this code as needed.
+            DentalHospitalDatabase.HospitalDataSetTableAdapters.schedulingTableAdapter hospitalDataSetschedulingTableAdapter = new DentalHospitalDatabase.HospitalDataSetTableAdapters.schedulingTableAdapter();
+            hospitalDataSetschedulingTableAdapter.Fill(hospitalDataSet.scheduling);
+            System.Windows.Data.CollectionViewSource schedulingViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("schedulingViewSource")));
+            schedulingViewSource.View.MoveCurrentToFirst();
         }
     }
 }
